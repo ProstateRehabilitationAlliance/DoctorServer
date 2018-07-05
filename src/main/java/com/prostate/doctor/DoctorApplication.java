f244457bc6c373ca50760c31a8555b378ea0840f
MaxCoderCh <MaxCoderCh@users.noreply.github.com>
2018-07-05 18:30:43 +0800

    添加 RPC远程 调用支持 注解

--- src/main/java/com/prostate/doctor/DoctorApplication.java
+++ src/main/java/com/prostate/doctor/DoctorApplication.java
@@ -4,7 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableHystrix
+@EnableFeignClients
 @EnableEurekaServer
 @SpringBootApplication
 @MapperScan(basePackages = {"com.prostate.doctor.mapper"})
