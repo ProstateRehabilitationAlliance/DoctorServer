0239a996858e4659864493aebf040de54e2f2e18
MaxCoderCh <MaxCoderCh@users.noreply.github.com>
2018-08-07 19:18:25 +0800

    修改密码验证规则
    修改密码验证规则

--- src/main/java/com/prostate/doctor/param/DoctorRegisteParams.java
+++ src/main/java/com/prostate/doctor/param/DoctorRegisteParams.java
@@ -8,7 +8,7 @@ public class DoctorRegisteParams implements Serializable{
 
     @Pattern(regexp = "((13[0-9])|(14[5,7,9])|(15([0-3]|[5-9]))|(17[0,1,3,5,6,7,8])|(18[0-9])|(19[8|9]))\\d{8}$", message = "请填入正确的手机号")
     private String doctorPhone;
-    @Pattern(regexp = "^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{8,16}$", message = "只能由8-16位数字或这字母组成")
+    @Pattern(regexp = "^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,10}$", message = "只能由6-10位数字或这字母组成")
     private String doctorPassword;
     @Size(max = 6, min = 6, message = "验证码格式不正确")
     private String smsCode;
