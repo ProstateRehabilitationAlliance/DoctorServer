9dc3d0d0112f7a37fa12fc93b84c923ddfbf365c
MaxCoderCh <MaxCoderCh@users.noreply.github.com>
2018-08-08 17:14:58 +0800

    认证信息接口开发
    添加认证信息查询接口
    添加认证信息修改接口

--- src/main/java/com/prostate/doctor/controller/DoctorSignController.java
+++ src/main/java/com/prostate/doctor/controller/DoctorSignController.java
@@ -1,14 +1,13 @@
 package com.prostate.doctor.controller;
 
-import com.prostate.doctor.cache.redis.RedisSerive;
 import com.prostate.doctor.entity.Doctor;
-import com.prostate.doctor.entity.DoctorDetail;
 import com.prostate.doctor.entity.DoctorSign;
 import com.prostate.doctor.feignService.ThirdServer;
 import com.prostate.doctor.service.DoctorDetailService;
 import com.prostate.doctor.service.DoctorSignService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,9 +27,6 @@ public class DoctorSignController extends BaseController {
     private DoctorDetailService doctorDetailService;
 
     @Autowired
-    private RedisSerive redisSerive;
-
-    @Autowired
     private ThirdServer thirdServer;
 
 
@@ -38,14 +34,14 @@ public class DoctorSignController extends BaseController {
      * 医生提交认证资料
      *
      * @param doctorSign
-     * @param token
      * @return
      */
     @PostMapping(value = "add")
-    public Map add(@Valid DoctorSign doctorSign, String token) {
+    public Map add(@Valid DoctorSign doctorSign) {
 
         log.info(doctorSign.toString());
-        Doctor doctor = redisSerive.getDoctor(token);
+
+        Doctor doctor = redisSerive.getDoctor();
 
         doctorSign.setDoctorId(doctor.getId());
         doctorSign.setId(doctor.getId());
@@ -56,59 +52,39 @@ public class DoctorSignController extends BaseController {
         return insertFailedResponse("认证资料提交失败");
     }
 
+
     /**
-     * 认证信息审核通过
+     * 查询认证信息
+     * @return
      */
-    @PostMapping(value = "agree")
-    public Map agree(String doctorSignId) {
+    @GetMapping(value = "get")
+    public Map get() {
+        Doctor doctor = redisSerive.getDoctor();
 
-        //修改认证状态
-        DoctorSign doctorSign = new DoctorSign();
-        doctorSign.setId(doctorSignId);
-        doctorSign.setApproveStatus("1");
-        doctorSignService.updateSelective(doctorSign);
-        //查询认证信息
-        doctorSign = doctorSignService.selectById(doctorSignId);
-        //识别身份证信息
-        Map<String,Object> idCardMap = thirdServer.idCard(doctorSign.getIdCardFront());
-        //添加医生个人信息
-        DoctorDetail doctorDetail = new DoctorDetail();
-        doctorDetail.setHospitalId(doctorSign.getHospitalId());
-        doctorDetail.setBranchId(doctorSign.getBranchId());
-        doctorDetail.setTitleId(doctorSign.getTitleId());
-        doctorDetail.setId(doctorSign.getId());
-        log.info( idCardMap.toString());
-        Map<String, Object> idCardInfo = (Map<String, Object>) idCardMap.get("result");
+        DoctorSign doctorSign = doctorSignService.selectById(doctor.getId());
 
-        doctorDetail.setDoctorCardNumber( idCardInfo.get("id").toString());
-        doctorDetail.setDoctorName(idCardInfo.get("name").toString());
-        doctorDetail.setDoctorSex(idCardInfo.get("sex").toString());
-        doctorDetail.setDoctorAddress(idCardInfo.get("address").toString());
-
-        int i = doctorDetailService.insertSelective(doctorDetail);
-        if (i > 0) {
-            return updateSuccseeResponse("认证信息处理成功!");
+        if (doctorSign == null) {
+            return queryEmptyResponse();
         }
-        return updateFailedResponse("认证信息处理失败!");
+        return querySuccessResponse(doctorSign);
     }
 
+
     /**
-     * 认证信息审核失败
+     * 修改认证资料
+     * @param doctorSign
+     * @return
      */
+    @PostMapping(value = "update")
+    public Map update(DoctorSign doctorSign) {
+        Doctor doctor = redisSerive.getDoctor();
 
-    @PostMapping(value = "refuse")
-    public Map refuse(String doctorSignId) {
-
-        DoctorSign doctorSign = new DoctorSign();
-        doctorSign.setId(doctorSignId);
-        doctorSign.setApproveStatus("0");
-
+        doctorSign.setId(doctor.getId());
         int i = doctorSignService.updateSelective(doctorSign);
         if (i > 0) {
-            return updateSuccseeResponse("认证信息处理成功!");
+            return updateSuccseeResponse("认证资料提交成功");
         }
-        return updateFailedResponse("认证信息处理失败!");
+        return updateFailedResponse("认证资料提交失败");
     }
-
 
 }
